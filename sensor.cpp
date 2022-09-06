ccb936693aeb2a174c063ed709b03db745ad6f67
Hector McKimm <mckimmh@gmail.com>
2022-09-06 10:12:32 +0100

    complete sensor model script

--- sensor.cpp
+++ sensor.cpp
@@ -1,4 +1,7 @@
 /* Sensor Network Localization
+ *
+ * g++ -Wall -std=c++17 sensor.cpp -lm
+ * ./a.out
  */
 
 #include <adept.h>
@@ -9,6 +12,89 @@
 #include <vector>
 
 // Component i,j of the log-likelihood
+adept::adouble ll_ij(const adept::adouble x_i0,
+                     const adept::adouble x_i1,
+                     const adept::adouble x_j0,
+                     const adept::adouble x_j1,
+                     const int w_ij,
+                     const double y_ij);
+
+// Log-likelihood
+adept::adouble log_lik(const adept::adouble x[6][2],
+                       const std::vector<int>& w,
+                       const std::vector<double>& y);
+
+// Log-prior: product of independent Gaussians with variance 100
+adept::adouble log_prior(const adept::adouble x[6][2]);
+
+// Log-posterior
+double log_posterior(const std::vector<double>& state,
+                     std::vector<double>& grad,
+                     const std::vector<int>& w,
+                     const std::vector<double>& y);
+
+// Generate the data
+void generate_data(std::mt19937_64& gen,
+                   const double x[6][2],
+                   std::vector<int>& w,
+                   std::vector<double>& y);
+
+// Numeric Differentiation
+void num_diff(std::vector<double>& state,
+              const double epsilon,
+              std::vector<double>& num_grad,
+              const std::vector<int>& w,
+              const std::vector<double>& y);
+
+// Print vector
+void print_vector(const char* expression,
+                  const std::vector<double>& v);
+
+int main()
+{
+    const int seed = 576;
+    std::mt19937_64 gen(seed);
+    
+    const double x[6][2] = {{0.57, 0.91},
+                            {0.10, 0.37},
+                            {0.26, 0.14},
+                            {0.85, 0.04},
+                            {0.50, 0.30},
+                            {0.30, 0.70}};
+    
+    std::vector<int> w;      // Vector of which distances are observed
+    std::vector<double> y;   // Vector of observations
+    
+    generate_data(gen, x, w, y);
+    
+    std::normal_distribution<double> rnorm(0.0, 1.0);
+    
+    // State at which to evaluate the log-density
+    std::vector<double> state;
+    for (int i = 0; i < 8; i++){
+        double state_i = rnorm(gen);
+        state.push_back(state_i);
+    }
+    
+    // Gradient
+    std::vector<double> grad(8, 0.0);
+    
+    double ld = log_posterior(state, grad, w, y);
+    
+    std::cout << "Log-density: " << ld << '\n';
+    
+    print_vector("Gradient        : ", grad);
+    
+    // Numeric Differentiation
+    const double epsilon = 0.00001;
+    std::vector<double> num_grad(8, 0.0);
+    num_diff(state, epsilon, num_grad, w, y);
+    
+    print_vector("Numeric Gradient: ", num_grad);
+     
+    return 0;
+}
+
 adept::adouble ll_ij(const adept::adouble x_i0,
                      const adept::adouble x_i1,
                      const adept::adouble x_j0,
@@ -33,7 +119,6 @@ adept::adouble ll_ij(const adept::adouble x_i0,
     return ll;
 }
 
-// Log-likelihood
 adept::adouble log_lik(const adept::adouble x[6][2],
                        const std::vector<int>& w,
                        const std::vector<double>& y)
@@ -55,7 +140,6 @@ adept::adouble log_lik(const adept::adouble x[6][2],
     return ll;
 }
 
-// Log-prior: product of independent Gaussians with variance 100
 adept::adouble log_prior(const adept::adouble x[6][2])
 {
     adept::adouble lp = 0;
@@ -68,11 +152,10 @@ adept::adouble log_prior(const adept::adouble x[6][2])
     return lp;
 }
 
-// Log-posterior
-adept::adouble log_posterior(const std::vector<double>& state,
-                             std::vector<double>& grad,
-                             const std::vector<int>& w,
-                             const std::vector<double>& y)
+double log_posterior(const std::vector<double>& state,
+                     std::vector<double>& grad,
+                     const std::vector<int>& w,
+                     const std::vector<double>& y)
 {
     if (state.size() != 8){
         std::cerr << "Error in log_posterior: state.size() != 8\n";
@@ -83,10 +166,10 @@ adept::adouble log_posterior(const std::vector<double>& state,
     
     adept::Stack stack;
     
-    const adept::adouble x[6][2] = {{state(0), state(1)},
-                                    {state(2), state(3)},
-                                    {state(4), state(5)},
-                                    {state(6), state(7)},
+    const adept::adouble x[6][2] = {{state[0], state[1]},
+                                    {state[2], state[3]},
+                                    {state[4], state[5]},
+                                    {state[6], state[7]},
                                     {0.50    , 0.30},
                                     {0.30    , 0.70}};
     
@@ -101,26 +184,18 @@ adept::adouble log_posterior(const std::vector<double>& state,
     
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 2; j++){
-            grad[2*i + j] = arr[i][j].get_gradient();
+            grad[2*i + j] = x[i][j].get_gradient();
         }
     }
     
     return ld.value();
 }
 
-int main()
+void generate_data(std::mt19937_64& gen,
+                   const double x[6][2],
+                   std::vector<int>& w,
+                   std::vector<double>& y)
 {
-    const int n_sensors = 6;
-    
-    const double x[6][2] = {{0.57, 0.91},
-                            {0.10, 0.37},
-                            {0.26, 0.14},
-                            {0.85, 0.04},
-                            {0.50, 0.30},
-                            {0.30, 0.70}};
-    
-    int seed = 576;
-    std::mt19937_64 gen(seed);
     std::uniform_real_distribution<double> runif(0.0, 1.0);
     std::normal_distribution<double> rnorm(0.0, 1.0);
     
@@ -129,7 +204,7 @@ int main()
     for (int i = 0; i < 4; i++){
         for (int j = i+1; j < 6; j++){
             double m = pow(x[i][0] - x[j][0], 2.0) + pow(x[i][1] - x[j][1],2.0);
-            m_mat[i,j] = m;
+            m_mat[i][j] = m;
         }
     }
     
@@ -141,57 +216,72 @@ int main()
         int row_sum = 0;
         for (int j = i+1; j < 6; j++)
         {
-            double p = exp(-50.0 * m_mat[i,j] / 9.0);
+            double p = exp(-50.0 * m_mat[i][j] / 9.0);
             double u = runif(gen);
             if (u < p)
             {
-                w_mat[i,j] = 1;
+                w_mat[i][j] = 1;
                 row_sum++;
             }
         }
         if (row_sum > 0) i++;
     }
     
-    std::vector<int> w;      // Vector of which distances are observed
-    std::vector<double> y;   // Vector of observations
     for (int i = 0; i < 4; i++){
         for (int j = i+1; j < 6; j++){
-            double w_ij = w_mat[i,j];
+            double w_ij = w_mat[i][j];
             w.push_back(w_ij);
             
             if (w_ij == 1)
             {
                 double y_ij = rnorm(gen);
                 y_ij *= 0.02;
-                y_ij += sqrt(m_mat[i,j]);
+                y_ij += sqrt(m_mat[i][j]);
                 y.push_back(y_ij);
             } else {
                 y.push_back(0);
             }
         }
     }
-    
-    // State at which to evaluate the log-density
-    std::vector<double> state;
-    for (int i = 0; i < 8; i++){
-        double state_i = rnorm(gen);
-        state.push_back(state_i);
+}
+
+void num_diff(std::vector<double>& state,
+              const double epsilon,
+              std::vector<double>& num_grad,
+              const std::vector<int>& w,
+              const std::vector<double>& y)
+{
+    if (state.size() != 8){
+        std::cerr << "Error in num_diff: state.size() != 8\n";
+    }
+    if (epsilon <= 0){
+        std::cerr << "Error in num_diff: epsilon <= 0\n";
+    }
+    if (num_grad.size() != 8){
+        std::cerr << "Error in num_diff: num_grad.size() != 8\n";
     }
     
-    // Gradient
-    std::vector<double> grad(8, 0.0);
+    std::vector<double> grad_aux(8, 0.0);
     
-    double ld = log_posterior(state, grad, w, y);
+    const double ld = log_posterior(state, grad_aux, w, y);
     
-    std::cout << "Log-density: " << ld << '\n';
-    std::cout << "Gradient: ";
-    for (std::vector<double>::iterator it = grad.begin();
-         it != grad.end(); i++){
+    for (int i = 0; i < 8; i++)
+    {
+        state[i] += epsilon;
+        double ld_epsilon = log_posterior(state, grad_aux, w, y);
+        state[i] -= epsilon;
+        
+        num_grad[i] = (ld_epsilon - ld) / epsilon;
+    }
+}
+
+void print_vector(const char* expression,
+                  const std::vector<double>& v)
+{
+    std::cout << expression;
+    for (std::vector<double>::const_iterator it = v.begin();
+         it != v.end(); ++it){
         std::cout << *it << ' ';
     }
     std::cout << '\n';
-    
-    
-    
-    return 0;
 }
